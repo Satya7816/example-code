25e3155d582cc9190c479b551a490ed7369f02de
Your Name <you@example.com>
2023-08-28 13:57:30 +0530

    commented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Nav/Nav';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
 import './App.css';
-
+//ytevby6ygnu,kuh
 function App() {
   return (
     <div className="App">
